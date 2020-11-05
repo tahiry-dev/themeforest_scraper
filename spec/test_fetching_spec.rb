7a8bcbc491b1788b4b8f5baa3f5993346d8b9f25
tahiry-dev <tahirycontact@gmail.com>
2020-11-05 08:52:54 +0300

    add test for the new instance method

--- spec/test_fetching_spec.rb
+++ spec/test_fetching_spec.rb
@@ -2,6 +2,7 @@ require_relative '../lib/fetching'
 
 RSpec.describe Fetching do
   let(:my_choice) { Fetching.new('Corporate', 46) }
+  let(:corporate) {Fetching.new('Corporate', 70)}
 
   describe '#instance' do
     it 'should return the categories and the budget' do
@@ -14,5 +15,11 @@ RSpec.describe Fetching do
     it "should return the link of 'Creative'" do
       expect(my_choice.assign_url).is_a?(String)
     end
+  end
+
+  describe "#parse_data" do
+      it "should return an array" do
+        expect(corporate.parse_data('https://themeforest.net/category/wordpress/corporate?&sort=sales')).is_a?(Array)
+      end
   end
 end
