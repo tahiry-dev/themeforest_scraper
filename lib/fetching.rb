736ac0e886798e1fd3ba3601ce8ee141eb4d49d9
tahiry-dev <tahirycontact@gmail.com>
2020-11-05 05:44:42 +0300

    use variable on link in fetching file to avoid repetition

--- lib/fetching.rb
+++ lib/fetching.rb
@@ -7,31 +7,6 @@ class Fetching
   end
 
   def assign_url
-    if @category == 'Corporate'
-      @url = 'https://themeforest.net/category/wordpress/corporate?&sort=sales'
-    elsif @category == 'Creative'
-      @url = 'https://themeforest.net/category/wordpress/creative?&sort=sales'
-    elsif @category == 'E-commerce'
-      @url = 'https://themeforest.net/category/wordpress/ecommerce?&sort=sales'
-    elsif @category == 'Entertainment'
-      @url = 'https://themeforest.net/category/wordpress/entertainment?&sort=sales'
-    elsif @category == 'Technology'
-      @url = 'https://themeforest.net/category/wordpress/technology?&sort=sales'
-    elsif @category == 'Nonprofit'
-      @url = 'https://themeforest.net/category/wordpress/nonprofit?&sort=sales'
-    elsif @category == 'Education'
-      @url = 'https://themeforest.net/category/wordpress/education?&sort=sales'
-    elsif @category == 'Real Estate'
-      @url = 'https://themeforest.net/category/wordpress/real-estate?&sort=sales'
-    elsif @category == 'Miscellaneous'
-      @url = 'https://themeforest.net/category/wordpress/miscellaneous?&sort=sales'
-    elsif @category == 'Wedding'
-      @url = 'https://themeforest.net/category/wordpress/wedding?&sort=sales'
-    elsif @category == 'BuddyPress'
-      @url = 'https://themeforest.net/category/wordpress/buddypress?&sort=sales'
-    elsif @category == 'Mobile'
-      @url = 'https://themeforest.net/category/wordpress/mobile?&sort=sales'
-    end
-    @url
+    @url = "https://themeforest.net/category/wordpress/#{@category}?&sort=sales" 
   end
 end
